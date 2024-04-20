e8ed7b046b4cc692647aef4d128917acec87a1e5
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-04-20 14:57:33 +0530

    this is my second code

---
+++ sum.c
@@ -0,0 +1,11 @@
+//it is the sum of two number
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    sum = a + b;
+    printf("%d",sum);
+    return 0;
+}
