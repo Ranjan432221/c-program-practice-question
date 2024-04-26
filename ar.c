e3d229d302c68f20602c9d8a131cb6e3ecfcaa7f
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-04-27 03:05:24 +0530

    it is a simple array print

---
+++ ar.c
@@ -0,0 +1,11 @@
+//it is a simple question of array print in c programming language
+#include<stdio.h>
+void main()
+{
+    int ar[5]={23,45,34,56};
+    int i;
+    for(i=0;i<=3;i++)
+    {
+        printf("enter at ar[%d]=%d\n",i,ar[i]);
+    }
+}  
