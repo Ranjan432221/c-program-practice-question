64c2765ccbb1049603d2012776921bf5c44b7ec6
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-04-28 01:30:43 +0530

    it is thesimple array question

---
+++ ar1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a[3],i;
+  printf("enter element 1:");
+  scanf("%d",&a[0]);
+  printf("enter element 2:");
+  scanf("%d",&a[1]);
+  printf("eneter element 3:");
+  scanf("%d",&a[2]);
+  for(i=0;i<=2;i++)  
+  {
+    printf("value at a[%d]=%d\n",i,a[i]);
+  }
+}
