2a68705a679f29afd8dfd8f15b067e3d3737593e
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-04-29 22:31:51 +0530

    it s simple array print

---
+++ ar2.c
@@ -0,0 +1,9 @@
+//it is the simple array printf
+#include<stdio.h>
+void main()
+{
+    int i,a[5]={45,56,67};
+    printf("value of a[0]=%d\n",a[0]);
+    printf("value of a[1]=%d\n",a[1]);
+    printf("value of a[2]=%d\n",a[2]);
+}
