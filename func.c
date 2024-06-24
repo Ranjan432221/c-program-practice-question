03d62fe3dc17f33da59222475839cd8d14aee7ed
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-06-24 22:01:02 +0530

    it is a c program

---
+++ func.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int x,int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+void swap(int,int);
+void main()
+{
+    int a= 5,b =7;
+    printf("before swapping a = %d, b= %d",a,b);
+    swap(a,b);
+    printf("after swapping a = %d,b=  %d",a,b);
+}
